797a8c0939902b1a9d1d4a2557210ec3e43afa0a
guilherme.adaniya <tadaadaniya@gmail.com>
2020-07-26 15:30:10 -0300

    Implementação listagem e inclusão de transações

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,23 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const totalIncome = transactions.reduce((total, next) => {
+      if (next.type === 'income') {
+        return total + Number(next.value);
+      }
+      return total;
+    }, 0);
+
+    const totalOutcome = transactions.reduce((total, next) => {
+      if (next.type === 'outcome') {
+        return total + Number(next.value);
+      }
+      return total;
+    }, 0);
+
+    const total = totalIncome - totalOutcome;
+    return { income: totalIncome, outcome: totalOutcome, total };
   }
 }
 
